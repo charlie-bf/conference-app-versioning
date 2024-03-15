5cfadd73b1b679a5f298748292909c8ba53106ff
charlie-bf <151586052+charlie-bf@users.noreply.github.com>
2024-03-15 11:08:11 +1000

    fix formatting

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 'use strict'
 let http = require('http');
 
-var AppVersion = "1.0.2"
+var AppVersion = "1.0.2"; // update this for each app version released
 
 let server = http.createServer(function (req, res) {
-  if( req.method === 'GET' ) {
+  if (req.method === 'GET') {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(AppVersion);
 		console.log(`Request Received, Response sent`)
   } else {
     res.writeHead(405, {'Content-Type': 'text/plain'});
-    res.end('501');
-		console.log('Error with request or response.')
+    res.end('Error');
+		console.log('Error with request.')
   }
 });
 server.listen(3000);
 console.log('Server running on port 3000');
+
+
