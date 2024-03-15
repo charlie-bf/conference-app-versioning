905ffa83a521b078ead93f3dc7b2f311d5d27b38
charlie-bf <151586052+charlie-bf@users.noreply.github.com>
2024-03-15 10:49:53 +1000

    Update to return appVersion and an error message

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 'use strict'
 let http = require('http');
+
+var AppVersion = "1.0.2"
+
+
 let server = http.createServer(function (req, res) {
   if( req.method === 'GET' ) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, World!\n');
-		console.log(`Request Received`)
+    res.end(AppVersion);
+		console.log(`Request Received, Response sent`)
   } else {
     res.writeHead(405, {'Content-Type': 'text/plain'});
-    res.end('Error');
+    res.end('501');
 		console.log('NOT WORKING')
   }
 });
 server.listen(3000);
-console.log('Server running on port 8000');
+console.log('Server running on port 3000');
